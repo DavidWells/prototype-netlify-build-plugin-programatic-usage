0bd0363d49ce73e0462c36cf6995428d8619a4d2
DavidWells <hello@davidwells.io>
2020-01-10 12:56:00 -0800

    add sitemap plugin

--- plugins/orchestrate/index.js
+++ plugins/orchestrate/index.js
@@ -1,27 +1,27 @@
+/* Include plugins for programmatic usage.
+  Note these are local plugins but could be normal NPM packages
+*/
 const pluginOne = require('../one')
 const pluginTwo = require('../two')
-const pluginThree = require('../three') // simple object
+const pluginThree = require('../three')
 
 module.exports = function orchestratorPlugin(config) {
-  // initialize plugins with config (if they have config)
-  const one = pluginOne({
-    optionOne: config.valueOne
-  })
-  const two = pluginTwo({
-    other: config.valueTwo
-  })
+  /* initialize plugins with config (if they have config) */
+  const one = pluginOne({ optionOne: config.valueOne })
+  const two = pluginTwo({ other: config.valueTwo })
 
   return {
     name: 'netlify-plugin-orchestrate',
     onInit: async (context) => {
-      // these plugins can run in parallel to speed things up
-      const [db, files] = await Promise.all([
+      /* Run plugin 1 & 2 in parallel to speed things up */
+      const [ db, files ] = await Promise.all([
         one.onInit(context),
         two.onInit(context),
       ])
-      // Programatically run onInit (could be any method exposed)
-      const output = await pluginThree.onInit(context, db, files)
-      console.log(`plugin-orchestrate: Output from plugin 3 = ${JSON.stringify(output)}`)
+      /* Take output from plugin 1 & 2 and run plugin 3 */
+      const outputFromThree = await pluginThree.onInit(context, db, files)
+      console.log('netlify-plugin-orchestrate output from pluginThree.onInit')
+      console.log(JSON.stringify(outputFromThree))
     },
   }
 }
